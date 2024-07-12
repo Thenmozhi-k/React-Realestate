97b46c2374faa9aa8d6835f2bbadb5dd6a290f57
Thenmozhi <jothi552004@gmail.com>
2024-07-13 01:00:05 +0530

    first commit

--- src/Components/Pin.jsx
+++ src/Components/Pin.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Import the default marker icon
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import markerRetina from 'leaflet/dist/images/marker-icon-2x.png';
+
+// Set default icon
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerRetina,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const Pin = ({ item }) => {
   return (
     <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div>
-         <img src={item.img} alt="" />
-         <div>
-          <Link to={`/${item.id}`}>{item.title}</Link>
-          <p>{item.bedrooms} Bedrooms</p>
-         </div>
-          
-          
+          <img src={item.img} alt="" />
+          <div>
+            <Link to={`/${item.id}`}>{item.title}</Link>
+            <p>{item.bedrooms} Bedrooms</p>
+          </div>
           <p>Price: ${item.price.toLocaleString()}</p>
         </div>
       </Popup>
